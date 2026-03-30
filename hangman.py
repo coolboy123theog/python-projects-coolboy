33209a63bc59eed245f3c076b63b5d203eeec17c
coolboy123theog <oulabirania@gmail.com>
2026-03-30 19:07:00 +0200

    Add files via upload

--- hangman.py
+++ hangman.py
@@ -1,9 +1,112 @@
 word="python"
 list=["_"]*len(word)
-print("pick a letter")
-ans=input()
-if ans in word:
-    for i in range(len(word)):
-        if word[i]==ans:
-            list [i]=ans
-print(" ".join(list))
+print('''
+___________
+|      |
+|      O
+|     \\|/
+|     / \\
+|
+      ''')
+stages=[
+
+    '''
+
+|      
+|      
+|     
+|     
+|
+''',
+'''
+___________
+|      
+|      
+|     
+|     
+|
+''',
+'''
+___________
+|      |
+|      
+|     
+|     
+|
+''',
+'''
+___________
+|      |
+|      O
+|     
+|     
+|
+''',
+'''
+___________
+|      |
+|      O
+|      |
+|     
+|
+'''
+,
+'''
+___________
+|      |
+|      O
+|     \\|
+|     
+|
+'''
+,
+'''
+___________
+|      |
+|      O
+|     \\|/
+|     
+|
+'''
+,
+'''
+___________
+|      |
+|      O
+|     \\|/
+|     / 
+|
+'''
+,
+'''
+___________
+|      |
+|      O
+|     \\|/
+|     / \\
+|
+'''
+
+]
+count=0
+
+while True:
+    print("pick a letter")
+    ans=input()
+    if ans in word:
+        for i in range(len(word)):
+            if word[i]==ans:
+                list [i]=ans
+    else:
+        print("your guess is incorrect")
+        print (stages[count])
+        count=count+1
+        print(count)
+        if count==9:
+            print("you lost")
+            break
+    print(" ".join(list))
+    
+    if "".join(list)==word:
+        print("you did it!")
+        break
